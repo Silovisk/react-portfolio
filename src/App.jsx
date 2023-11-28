24e750df0d7dfb0f6416a0771464d7e666ed2ac2
Silovisk <silasaraujo1@hotmail.com>
2023-11-28 20:04:16 -0300

    update HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import Contact from "./components/Contact";
 import Hero from "./components/Hero";
 import Who from "./components/Who";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom"; // Mudança para HashRouter
 
 const Container = styled.div`
   height: 100vh;
